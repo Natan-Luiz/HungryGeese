11c15f092971e9c22cc3e4fdf89491524757bf66
Natan-Luiz <37088299+Natan-Luiz@users.noreply.github.com>
2021-02-26 01:32:50 -0300

    Update BasicCode.py

--- BasicCode.py
+++ BasicCode.py
@@ -1,8 +1,7 @@
 from kaggle_environments.envs.hungry_geese.hungry_geese import Observation, Configuration, Action, row_col
 from kaggle_environments import evaluate, make, utils
-import random
 
-# Cada agente precisa ter sua função
+# Cada agente precisa ter sua função, é aqui dentro que temos que encapsular todo o bot
 def agent1(obs_dict, config_dict):
     return Action.SOUTH.name
 
@@ -10,6 +9,7 @@ def agent2(obs_dict, config_dict):
     return Action.NORTH.name
 
 
+#Funções que rodam o jogo
 env = make("hungry_geese", debug=True)
 env.run([agent1, agent2]) # Aqui escolhe o numero de agentes e a função que cada um vai utilizar
 env.render(mode="ipython", width=500, height=350)
